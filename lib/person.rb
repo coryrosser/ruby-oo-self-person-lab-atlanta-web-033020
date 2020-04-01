ecbd0cfc8a9d872880537bcae2d71f6d7aaa4688
Cory Rosser <coryrosser@gmail.com>
2020-04-01 17:10:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,43 @@
 # your code goes here
+
+class Person 
+  attr_reader :name
+  attr_accessor :bank_account
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  
+  
+  
+  
+  
+  def happiness=(value)
+    if value > 10 
+      @happiness = 10 
+      elsif value < 0 
+      @happiness = 0 
+    else 
+      @happiness = value
+    end 
+    @happiness
+  end
+  
+  def hygiene=(value)
+    if value > 10 
+      @hygiene = 10 
+      elsif value < 0 
+      @hygiene = 0 
+    else 
+    @hygiene = value
+    
+  end
+  def clean(hygiene)
+    @hygiene > 7
+  end
+  
+end 
